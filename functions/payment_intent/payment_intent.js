fdc92cf882ae68af8e741cfd688577a77fd1279f
Syed Ali Uz Zaman <45460148+alyaxi@users.noreply.github.com>
2021-07-13 20:16:31 +0500

    Setup Stripe with Server Rendering

---
+++ functions/payment_intent/payment_intent.js
@@ -0,0 +1,23 @@
+// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+const stripe = require('stripe')('sk_test_51JCWiQL5SsXqkixG1S7EG4sDE56s0sjCk5sJnNkbF5dz7REFyOkVfDhqSwv6URG27ivG8W2A8kMZMo4hu4AjG5CD00Eh3Kp2iZ');
+const handler = async (event) => {
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: 800,
+      currency: 'usd',
+      // Verify your integration in this guide by including this parameter
+      metadata: {integration_check: 'accept_a_payment'},
+    });
+    return {
+      statusCode: 200,
+      body: JSON.stringify({client_secret: paymentIntent.client_secret} ),
+      // // more keys you can return:
+      // headers: { "headerName": "headerValue", ... },
+      // isBase64Encoded: true,
+    }
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() }
+  }
+}
+
+module.exports = { handler }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js"
+import CheckoutForm from "../components/CheckoutForm"
+
+const promise = loadStripe("pk_test_51JCWiQL5SsXqkixGf8QU3gRtUY1dzWgPvufnXW83OavRkYeAUAPY0XDpmR0UIuJK30NvLWv5unI9hSVJx1tdmtnV00M9IcjDTN");
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div>
+    <Elements stripe={promise}>
+        <CheckoutForm />
+      </Elements>
+  </div>
 }
