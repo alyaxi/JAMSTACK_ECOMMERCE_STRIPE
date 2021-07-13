c3df6324404ff9f09ed7d2223d5966c04b6f1356
Syed Ali Uz Zaman <45460148+alyaxi@users.noreply.github.com>
2021-07-14 03:52:44 +0500

    Setup Gatsby with SnipCartv3

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,23 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
+require("dotenv").config()
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+      siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+      {
+        resolve: `gatsby-source-stripe`,
+        options: {
+          objects: ["Price"],
+          secretKey: process.env.STRIPE_SECRET_KEY,
+          downloadFiles: false,
+        },
+      },
+      {
+        resolve: 'gatsby-plugin-snipcartv3',
+        options: {
+          apiKey: process.env.SNIPCART_SECRET_KEY
+        }
+      }
+    ],
 }
