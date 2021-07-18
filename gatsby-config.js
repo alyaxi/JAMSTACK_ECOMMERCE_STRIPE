8dd51c230b74c3bf02fe25a3e8897e43dc6c5a12
Syed Ali Uz Zaman <45460148+alyaxi@users.noreply.github.com>
2021-07-18 15:47:54 +0500

    Setup Project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,16 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`
+})
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-stripe`,
+      options: {
+        objects: ["Price"],
+        secretKey: process.env.GATSBY_STRIPE_SECRET,
+        downloadFiles: false,
+      },
+    },
+  ],
 }
